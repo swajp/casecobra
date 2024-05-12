fb854fc433ee8fb348f8a47afcebc85aa30cceef
swajp <stejskalmira@icloud.com>
2024-05-12 12:02:04 +0200

    phones component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import Phone from "@/components/phone";
+import Reviews from "@/components/reviews";
 import { CheckIcon, StarIcon } from "lucide-react";
 
 export default function Home() {
@@ -101,6 +103,94 @@ export default function Home() {
             </div>
           </div>
         </MaxWidthWrapper>
+      </section>
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              What our{" "}
+              <span className="relative px-2">
+                customers{" "}
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+              </span>{" "}
+              say
+            </h2>
+            <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
+          </div>
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "I love my new phone case! The quality is amazing and the
+                  print is super clear.{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    I can't wait to order more{" "}
+                  </span>
+                  . Thank you"
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-1.png"
+                  alt="user"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Jonathan</p>
+                  <div className="flex gap-1.5 items-center text-center text-zinc-600">
+                    <CheckIcon className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm"> Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "I usually keep my phone together with my keys in my pocket
+                  and that led to some pretty heavy scratches on my old case.
+                  I'm happy to say that my new CaseCobra{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    {" "}
+                    case is still looking brand new after a month of use
+                  </span>
+                  . I'm very happy with my purchase."
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-4.jpg"
+                  alt="user"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Miroslav</p>
+                  <div className="flex gap-1.5 items-center text-center text-zinc-600">
+                    <CheckIcon className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm"> Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+        <div className="pt-16">
+          <Reviews />
+        </div>
       </section>
     </div>
   );
