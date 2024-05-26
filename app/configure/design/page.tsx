678fc7a9d2ebc1973f5faa4af5951be8389a9b15
swajp <stejskalmira@icloud.com>
2024-05-26 15:54:22 +0200

    configurator updated

---
+++ app/configure/design/page.tsx
@@ -0,0 +1,34 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./_components/design-configurator";
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+export default async function DesignPage({ searchParams }: PageProps) {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+  return (
+    <DesignConfigurator
+      configId={id}
+      imageUrl={imageUrl}
+      imageDimensions={{ width, height }}
+    />
+  );
+}
---
+++ components/handle.tsx
@@ -0,0 +1,5 @@
+export default function Handle() {
+  return (
+    <div className="w-5 h-5 rounded-full shadow border bg-white border-zinc-200 transition hover:bg-primary" />
+  );
+}
