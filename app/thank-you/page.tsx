830662deb66e62981a1ec99b7cdc772b328d5dee
swajp <stejskalmira@icloud.com>
2024-06-02 13:23:52 +0200

    thank you page

---
+++ app/thank-you/page.tsx
@@ -0,0 +1,10 @@
+import { Suspense } from "react";
+import ThankYou from "./_component/thank-you";
+
+export default function ThankYouPage() {
+  return (
+    <Suspense>
+      <ThankYou />
+    </Suspense>
+  );
+}
