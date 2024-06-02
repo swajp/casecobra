9169be568ee996a73ca586ceea9ab3e9fea76c30
swajp <stejskalmira@icloud.com>
2024-06-02 16:02:14 +0200

    route fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -29,7 +29,7 @@ export default async function Navbar() {
                 </Link>
                 {isAdmin ? (
                   <Link
-                    href="/api/auth/logout"
+                    href="/dashboard"
                     className={buttonVariants({
                       size: "sm",
                       variant: "ghost",
