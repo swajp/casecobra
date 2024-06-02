dc4f1cbfba10062d1a5bbe6309537d429161f798
swajp <stejskalmira@icloud.com>
2024-06-02 16:01:59 +0200

    update status

---
+++ app/dashboard/_components/status-dropdown.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
+import { OrderStatus } from "@prisma/client";
+import { useMutation } from "@tanstack/react-query";
+import { CheckIcon, ChevronsUpDownIcon } from "lucide-react";
+import { changeOrderStatus } from "../actions";
+import { useRouter } from "next/navigation";
+
+const LABEL_MAP: Record<keyof typeof OrderStatus, string> = {
+  awaiting_shipment: "Awaiting shipment",
+  shipped: "Shipped",
+  fullfiled: "Fulfilled",
+};
+
+export default function StatusDropDown({
+  id,
+  orderStatus,
+}: {
+  id: string;
+  orderStatus: OrderStatus;
+}) {
+  const router = useRouter();
+  const { mutate } = useMutation({
+    mutationKey: ["change-order-status"],
+    mutationFn: changeOrderStatus,
+    onSuccess: () => router.refresh(),
+  });
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant={"outline"}
+          className="w-52 flex justify-between items-center"
+        >
+          {LABEL_MAP[orderStatus]}
+          <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-0">
+        {Object.keys(OrderStatus).map((status) => (
+          <DropdownMenuItem
+            onClick={() => mutate({ id, newStatus: status as OrderStatus })}
+            key={status}
+            className={cn(
+              "flex text-sm gap-1 items-center p-2.5 hover:bg-zinc-100",
+              {
+                "bg-zinc-100": orderStatus === status,
+              }
+            )}
+          >
+            <CheckIcon
+              className={cn("mr-2 h-4 w-4 text-primary opacity-0", {
+                "opacity-100": orderStatus === status,
+              })}
+            />
+            {LABEL_MAP[status as OrderStatus]}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
